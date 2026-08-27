4ab30ab71e7b87886f8a8c226af5c4c1e3816c93
MassayoshiOgawa <psnnapoleandro@gmail.com>
2026-08-26 21:46:00 -0300

    commit

--- desafioFinal/js/script.js
+++ desafioFinal/js/script.js
@@ -9,15 +9,38 @@ let pages = {
     "Matriz": document.getElementById("Matriz")
 }
 
+localStorage.setItem('currPage', 'inicio')
+
 Object.values(pages).forEach(element => {
-    element.style.display = "none"
+    if(element) element.style.display = "none"
 });
 
-pages["inicio"].style.display = "block"
+if(pages["inicio"]) pages["inicio"].style.display = "block"
+
+document.querySelectorAll('[id$="Att2"]').forEach(el => {
+    el.style.display = "none"
+});
 
 function switchPage(page){
     Object.values(pages).forEach(element => {
-        element.style.display = "none"
+        if(element) element.style.display = "none"
     });
-    pages[page].style.display = "block"
+    if(pages[page]) pages[page].style.display = "block"
+    localStorage.setItem('currPage', page)
+}
+
+function showAtt(AttId){
+    let currPage = localStorage.getItem('currPage')
+    let att1 = document.getElementById(`${currPage}Att1`)
+    let att2 = document.getElementById(`${currPage}Att2`)
+    if(AttId == 1){
+        if(att1) att1.style.display = "block"
+        if(att2) att2.style.display = "none"
+    }
+    if(AttId == 2){
+        if(att1) att1.style.display = "none"
+        if(att2) att2.style.display = "block"
+    }
+    let page = pages[currPage]
+    if(page) page.style.overflow = "visible"
 }
