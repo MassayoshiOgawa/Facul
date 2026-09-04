f8f4944d733a3773a5b364d4bc0f46baf311c7e5
MassayoshiOgawa <psnnapoleandro@gmail.com>
2026-09-04 15:52:23 -0300

    commit

--- desafioFinal_v2/atividades/javascript_for/for_att1/js/valproc.js
+++
@@ -1,7 +0,0 @@
-function validaFormulario(){
-
-    for(let i = 1; i <= 10; i++){
-        console.log(i)
-    }
-    return false
-}
--- desafioFinal_v2/atividades/javascript_for/for_att2/js/valproc.js
+++
@@ -1,7 +0,0 @@
-function validaFormulario() {
-    let num1 = parseInt(document.getElementById('num1').value);
-    for(let i = 1; i <= 10; i++){
-        console.log(`${num1} x ${i} = ${num1 * i}`)
-    }
-    return false;
-}
