0dd00fcf7049b2aa78fde642ddf8f4252365b293
MassayoshiOgawa <psnnapoleandro@gmail.com>
2026-09-09 13:29:11 -0300

    commit

--- faclube/script.js
+++ faclube/script.js
@@ -4,19 +4,29 @@ function validarForumalario(){
     let datadenascimento = document.getElementById('datenasc').value
     let email = document.getElementById('email').value
     let telefone = document.getElementById('telf').value
+    let participar = document.querySelector('input[name="participar"]').checked
 
-    if(nome === "" || !sexo || datadenascimento === "" || email === "" || telefone === "" || datadenascimento > new Date()){
+    if(nome === "" || !sexo || datadenascimento === "" || email === "" || telefone === ""){
         alert("Insira todos os campos.")
         return false
     }
 
     let expRegDataNasc = new RegExp(/^\d{2}\/\d{2}\/\d{4}$/)
-    let expRegEmail = new RegExp(/^\w+@\w+\.\w+$/)
-    let expRegTelefone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$")
+    let expRegEmail = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    let expRegTelefone = new RegExp(/^\(\d{2}\)\s?\d{4,5}-\d{4}$/)
+
     if(!expRegDataNasc.test(datadenascimento)){
         alert("Data de nascimento inválida. Use o formato 00/00/0000.")
         return false
     }
+
+    let partes = datadenascimento.split("/")
+    let dataNasc = new Date(partes[2], partes[1] - 1, partes[0])
+    if(dataNasc > new Date()){
+        alert("Data de nascimento não pode ser no futuro.")
+        return false
+    }
+
     if(!expRegEmail.test(email)){
         alert("Email inválido.")
         return false
@@ -25,5 +35,8 @@ function validarForumalario(){
         alert("Telefone inválido.")
         return false
     }
-
+    if(!participar){
+        alert("Confirme que deseja participar do fã-clube.")
+        return false
+    }
 }
